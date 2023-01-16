4b7d829444573cf1cbff1f84da95e3a76b67a163
Akeshihiro <akeshihiro@gmx.net>
2023-01-16 16:11:02 +0100

    Add some confort mappings to neovim

--- nvim/lua/config/default/remap.lua
+++ nvim/lua/config/default/remap.lua
@@ -5,6 +5,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Make Ctrl+C a full Esc
+vim.keymap.set("", "<C-c>", "<Esc>", { silent = true })
+
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
@@ -12,3 +15,9 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? \"gk\" : \"k\"", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? \"gj\" : \"j\"", { expr = true, silent = true })
+
+-- Make the jumping to the start and end of a line more comfortable
+-- (inspired by the Helix editor)
+vim.keymap.set("", "gh", "0", { silent = true })
+vim.keymap.set("", "gs", "^", { silent = true })
+vim.keymap.set("", "gl", "$", { silent = true })

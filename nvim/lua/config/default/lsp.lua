be8530172f248be35a64f77bb351c967b10d098a
Alexej Esau <aesau@dspace.de>
2025-05-12 07:55:53 +0200

    Fix nvim lsp config

--- nvim/lua/config/default/lsp.lua
+++ nvim/lua/config/default/lsp.lua
@@ -73,21 +73,21 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 require("mason").setup()
 
 -- Ensure the servers above are installed
-local mason_lspconfig = require "mason-lspconfig"
+local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+  ensure_installed = {},
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require("lspconfig")[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
+-- mason_lspconfig.setup_handlers {
+--   function(server_name)
+--     require("lspconfig")[server_name].setup {
+--       capabilities = capabilities,
+--       on_attach = on_attach,
+--       settings = servers[server_name],
+--     }
+--   end,
+-- }
 
 -- Turn on lsp status information
 require("fidget").setup()

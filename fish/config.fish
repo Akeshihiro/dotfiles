05a1559bc95285f73443916b44b108fdf8d5b748
Akeshihiro <akeshihiro@gmx.net>
2026-04-07 21:38:50 +0200

    Add bun to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,7 @@ set -gx EDITOR nvim
 set -gx VISUAL $EDITOR
 set -gx TERM xterm-256color
 
+fish_add_path ~/.bun/bin
 fish_add_path ~/.dotnet/tools
 if [ -d "/usr/lib/jvm/default" ]
     set -gx JAVA_HOME /usr/lib/jvm/default
